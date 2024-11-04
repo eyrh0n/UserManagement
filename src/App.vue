725af3bcabb791c25fcb921001ee52f1431d46b4
eyrh0n <senpaishane2@gmail.com>
2024-11-04 16:02:26 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import UserManagement from './components/user-management.vue';
+import userManagement from './components/user-management.vue';
+</script>
+
+<template>
+
+  <div id="app">
+    <UserManagement />
+
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
